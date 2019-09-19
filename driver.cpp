3c99bd023b81bb8270b213abc26587fa1a553497
sean-custer <34922998+sean-custer@users.noreply.github.com>
2019-09-18 23:34:08 -0400

    driver.cpp
    File to run the Sally FORTH interpreter

---
+++ driver.cpp
@@ -0,0 +1,17 @@
+// File: driver.cpp
+//
+//
+// Simple driver program to call the Sally Forth interpreter
+//
+
+
+#include <iostream>
+#include "Sally.h"
+
+int main() {
+   Sally S ;
+
+   S.mainLoop() ;
+
+   return 0 ;
+}
